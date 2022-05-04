355da9c3bf90f4454cdbe5d3610f86726d6df114
Joe Barchanowicz <joebarchanowicz@gmail.com>
2022-05-04 21:13:55 +1000

    Complete Hangman

---
+++ hangman/main.py
@@ -0,0 +1,42 @@
+from platform import java_ver
+import random
+from hangman_wordlist import word_list
+from game_art import stages, logo
+
+print(logo)
+game_is_finished = False
+lives = len(stages) - 1
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+word_length = len(chosen_word)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not game_is_finished:
+    guess = input("Guess a letter: ").lower()
+
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(f"{' '.join(display)}")
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            game_is_finished = True
+            print("You lose.")
+    
+    if not "_" in display:
+        game_is_finished = True
+        print("You win.")
+
+    print(stages[lives])
